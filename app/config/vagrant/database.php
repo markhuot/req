102eae3e1f597010fa3e446873a52ece6d589712
Mark Huot <mark@markhuot.com>
2014-07-02 07:38:42 -0400

    adding vagrant

---
+++ app/config/vagrant/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+  'default' => 'mysql',
+
+  'connections' => array(
+
+    'mysql' => array(
+      'driver'    => 'mysql',
+      'host'      => 'localhost',
+      'database'  => 'req',
+      'username'  => 'root',
+      'password'  => '',
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+    ),
+
+  ),
+
+);
--- app/database/seeds/RequirementSeeder.php
+++ app/database/seeds/RequirementSeeder.php
@@ -9,6 +9,7 @@ class RequirementTableSeeder extends Seeder {
     $testProject = Project::where('name', '=', 'Test Project')->firstOrFail()->id;
     $user = User::first();
     $lorem = ['Lorem','ipsum','dolor','sit','amet,','consectetur','adipisicing','elit,','sed','do','eiusmod','tempor','incididunt','ut','labore','et','dolore','magna','aliqua.','Ut','enim','ad','minim','veniam,','quis','nostrud','exercitation','ullamco','laboris','nisi','ut','aliquip','ex','ea','commodo','consequat.','Duis','aute','irure','dolor','in','reprehenderit','in','voluptate','velit','esse','cillum','dolore','eu','fugiat','nulla','pariatur.','Excepteur','sint','occaecat','cupidatat','non','proident,','sunt','in','culpa','qui','officia','deserunt','mollit','anim','id','est','laborum'];
+    $statuses = ['pending', 'accepted', 'rejected', 'delivered', 'closed'];
 
     Requirement::create([
       'project_id' => $testProject,
@@ -22,12 +23,13 @@ class RequirementTableSeeder extends Seeder {
       $title = implode(' ', array_slice($lorem, 0, rand(5,10)));
       shuffle($lorem);
       $body = implode(' ', array_slice($lorem, 0, rand(100,200)));
+      $statusKey = array_rand($statuses);
 
       $requirement = Requirement::create([
         'project_id' => $testProject,
         'title' => ucfirst($title),
         'body' => ucfirst($body),
-        'status' => 'accepted',
+        'status' => $statuses[$statusKey],
       ]);
 
       $requirement->assignments()->sync([$user->id]);
